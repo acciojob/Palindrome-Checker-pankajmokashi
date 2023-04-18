4e9a0082ee0a015ee161a17e98c6f0900277dffb
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-19 01:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+	let arr = str.split(" ")
+    let str1 = arr.join("")
+    arr = str1.split("")
+    let i = 0, j = arr.length - 1
+    while(i < j)
+    {
+        if(arr[i] != arr[j])
+        {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
 }
 module.exports = palindrome
